fc2ee550dfcb315ea0845136039cd044d53a77e8
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2026-05-01 21:11:11 -0700

    Add poison volley on Marli

--- DBM-Raids-Vanilla/VanillaZG/Marli.lua
+++ DBM-Raids-Vanilla/VanillaZG/Marli.lua
@@ -21,13 +21,14 @@ mod:SetZone(309)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED 24111 24300 24109",
+	"SPELL_AURA_APPLIED 24099 24111 24300 24109",
 	"SPELL_AURA_REMOVED 24111 24300",
 	"SPELL_CAST_SUCCESS 24083"
 )
 
 --TODO, enlarge dispel warning valid?
 local warnSpiders		= mod:NewSpellAnnounce(24083, 2)
+local warnPoisonVolley 	= mod:NewSpellAnnounce(24099, 2, nil, "RemovePoison")
 local warnDrain			= mod:NewTargetNoFilterAnnounce(24300, 2, nil, "RemoveMagic|Healer")
 local warnCorrosive		= mod:NewTargetNoFilterAnnounce(24111, 2, nil, "RemovePoison")
 local warnEnlarge		= mod:NewTargetNoFilterAnnounce(24109, 3)
@@ -38,7 +39,9 @@ local timerDrain		= mod:NewTargetTimer(7, 24300, nil, "RemoveMagic|Healer", nil,
 local timerCorrosive	= mod:NewTargetTimer(30, 24111, nil, "RemovePoison", nil, 5, nil, DBM_COMMON_L.POISON_ICON)
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args:IsSpell(24111) then
+	if ars:IsSpell(24099) and self:AntiSpam(3) then
+		warnPoisonVolley:Show()
+	elseif args:IsSpell(24111) then
 		warnCorrosive:Show(args.destName)
 		timerCorrosive:Start(args.destName)
 	elseif args:IsSpell(24300) and args:IsDestTypePlayer() then
