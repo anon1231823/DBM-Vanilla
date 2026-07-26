8d635ccaf1e28653251b1ecf23160ab0b2c6c097
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2026-07-26 09:30:37 -0700

    Thaddius polarity

--- DBM-Raids-Vanilla/VanillaNaxx/ConstructQuarter/Thaddius.lua
+++ DBM-Raids-Vanilla/VanillaNaxx/ConstructQuarter/Thaddius.lua
@@ -122,23 +122,15 @@ end
 
 function mod:UNIT_AURA()
 	if self:GetStage(2, 3) or (GetTime() - lastShift) > 5 or (GetTime() - lastShift) < 3 then return end
-	local charge
-	local chargeIcon
-	local i = 1
-	while UnitDebuff("player", i) do
-		local _, icon, count, _, _, _, _, _, _, _, _, _, _, _, _, count2 = UnitDebuff("player", i)
-		if icon == 135768 then
-			if (count2 or count) > 1 then return end
-			charge = CL.NEGATIVE
-			chargeIcon = tostring(icon)
-			yellShift:Yell(7, "- -")
-		elseif icon == 135769 then
-			if (count2 or count) > 1 then return end
-			charge = CL.POSITIVE
-			chargeIcon = tostring(icon)
-			yellShift:Yell(6, "+ +")
-		end
-		i = i + 1
+	local charge, chargeIcon
+	if DBM:UnitDebuff("player", 28084) then
+		charge = CL.NEGATIVE
+		chargeIcon = "135768"
+		yellShift:Yell(7, "- -")
+	elseif DBM:UnitDebuff("player", 28059) then
+		charge = CL.POSITIVE
+		chargeIcon = "135769"
+		yellShift:Yell(6, "+ +")
 	end
 	if charge then
 		lastShift = 0
