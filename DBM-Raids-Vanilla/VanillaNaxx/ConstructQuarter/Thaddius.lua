147255ac7a86c360898fd2fb46981406e799a99e
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2026-07-28 09:46:30 -0700

    space

--- DBM-Raids-Vanilla/VanillaNaxx/ConstructQuarter/Thaddius.lua
+++ DBM-Raids-Vanilla/VanillaNaxx/ConstructQuarter/Thaddius.lua
@@ -112,7 +112,7 @@ function mod:SPELL_CAST_START(args)
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpell(28338, 28339) and self:AntiSpam(3,1) then
+	if args:IsSpell(28338, 28339) and self:AntiSpam(3, 1) then
 		warnThrow:Show()
 		timerThrow:Start()
 		warnThrowSoon:Schedule(16)
